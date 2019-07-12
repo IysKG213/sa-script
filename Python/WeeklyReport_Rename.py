fe30b1090c76633557bbe6761d943d626c27bbbb
IysKG213 <49123957+IysKG213@users.noreply.github.com>
2019-07-12 17:06:11 +0900

    Update WeeklyReport_Rename.py

--- Python/WeeklyReport_Rename.py
+++ Python/WeeklyReport_Rename.py
@@ -11,16 +11,17 @@ for rep_name in os.listdir('.'):
         if ext == '.md':
             files.append(rep_name) 
 
-## リスト内最後尾(最新)のファイルを変数に代入
+## リスト内最新のファイルを変数に代入
 rep_name = files[-1]
 
-## 変数tdに今日の日付を代入
-td = datetime.date.today()
+## 今日の日付を変数tDayに代入
+tDay = datetime.date.today()
 
-## 変数tdに来週の7日分加算して変数ndとする
-nd = td + datetime.timedelta(days = 7)
-## 変数ndを整形しyyyymmdd形式にする
-nd = nd.strftime("%Y%m%d")
+## 次の木曜の日付を計算する(月曜=0～日曜=6)
+dayNum = 3 - tDay.weekday() 
+dayNum = dayNum+7 if dayNum < 0 else dayNum
+delta = datetime.timedelta(days=dayNum)
+nextDate = tDay + delta
 
 ## 取得した最新のファイルコピー元にして新しい定例資料を作成
 shutil.copy(rep_name,'定例資料_'+ nd +'.md')
